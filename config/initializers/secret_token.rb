77a2de686b9132487b3d84b8bea743e17409f48d
ankithbti <ankithbti007@gmail.com>
2012-10-20 02:17:48 +0530

    GeoCoder API demo in rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitescene::Application.config.secret_token = '7435867bbc5acd17f134ee74557e59df3058e0713bb32946e1b278f6e982f435ff0b69ebc973e3fa1874152d3f8020471008bd836e83611f07c0e0216a8484ac'
